f00e381e7a8c46ffbdfedaebb2c5b464ac26a7bc
Gustavo Sabino <sabino.gustavo.gs@gmail.com>
2022-05-30 14:28:39 -0300

    adjusting log print

--- app/conco.py
+++ app/conco.py
@@ -1,5 +1,6 @@
-import pywhatkit
+'''import pywhatkit'''
 import pandas as pd 
+import datetime
 
 
 clientes = pd.read_csv("base/base_robo_claro.csv", sep=";", encoding="UTF-8")
@@ -7,10 +8,11 @@ clientes = pd.read_csv("base/base_robo_claro.csv", sep=";", encoding="UTF-8")
 
 for cliente in range(len(clientes)):
 
-        mensagem = f"Está disponível para empresa, {clientes.loc[cliente, 'Razão Social']}, uma excelente condição, para que possa ser um cliente de telefonia móvel da Vivo Empresas.\n 1. Tenho interesse em saber mais;\n 2.Não tenho interesse em melhorar a condição do meu plano e tão pouco reduzir o valor da minha conta de telefonia móvel." 
+        mensagem = f"Está disponível para empresa, {clientes.loc[cliente, 'Razão Social']}, uma excelente condição, para que possa ser um cliente de telefonia móvel da Vivo Empresas.\n\n 1. Tenho interesse em saber mais;\n\n 2.Não tenho interesse em melhorar a condição do meu plano e tampouco reduzir o valor da minha conta de telefonia móvel." 
         
-        pywhatkit.sendwhatmsg_instantly(phone_no=f"+55{int(clientes.loc[cliente, 'Telefone de Contato'])}", message = mensagem, wait_time = 40, tab_close = True )
-        
-     
+        '''pywhatkit.sendwhatmsg_instantly(phone_no=f"+55{int(clientes.loc[cliente, 'Telefone de Contato'])}", message = mensagem, wait_time = 40, tab_close = True )'''
 
+        with open("log.txt", "a+") as file_object:
+            file_object.write(f"{cliente, clientes.loc[cliente,'CNPJ']} - {datetime.datetime.now()} \n")   
+        
 
