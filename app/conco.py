511ba27367da17f41b9458ca7432e10202b7bbb3
Gustavo Sabino <sabino.gustavo.gs@gmail.com>
2022-05-30 12:22:45 -0300

    adjusting conco bot

--- app/conco.py
+++ app/conco.py
@@ -6,13 +6,11 @@ clientes = pd.read_csv("base/base_robo_claro.csv", sep=";", encoding="UTF-8")
 
 
 for cliente in range(len(clientes)):
+
+        mensagem = f"Está disponível para empresa, {clientes.loc[cliente, 'Razão Social']}, uma excelente condição, para que possa ser um cliente de telefonia móvel da Vivo Empresas.\n 1. Tenho interesse em saber mais;\n 2.Não tenho interesse em melhorar a condição do meu plano e tão pouco reduzir o valor da minha conta de telefonia móvel." )
         
-              
-        mensagens = (
-           f"Está disponível para empresa, {clientes.loc[cliente, 'Razão Social']}, uma excelente condição, para que possa ser um cliente de telefonia móvel da Vivo Empresas.\n 1. Tenho interesse em saber mais;\n 2.Não tenho interesse em melhorar a condição do meu plano e tão pouco reduzir o valor da minha conta de telefonia móvel." )
-        for mensagem in mensagens:
-            pywhatkit.sendwhatmsg_instantly(phone_no=f"+55{int(clientes.loc[cliente, 'Telefone de Contato'])}", message = mensagem, wait_time = 40, tab_close = True )
-            
+        pywhatkit.sendwhatmsg_instantly(phone_no=f"+55{int(clientes.loc[cliente, 'Telefone de Contato'])}", message = mensagem, wait_time = 40, tab_close = True )
+        
      
 
 
